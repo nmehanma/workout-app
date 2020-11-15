86b58db3f5a5eaf8d2b2cf83b28867963692ed27
nmehanma <nmehanma@gmail.com>
2020-11-15 18:42:07 -0500

    replace hard coded date in create_exercise_spec file

--- spec/features/exercises/create_exercise_spec.rb
+++ spec/features/exercises/create_exercise_spec.rb
@@ -17,7 +17,7 @@ RSpec.feature "Creating Exercise" do
 
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight Lifting"
-    fill_in "Activity date", with: "2016-07-26"
+    fill_in "Activity date", with: 3.days.ago
     click_button "Create Exercise"
 
 
